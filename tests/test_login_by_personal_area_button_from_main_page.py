5183856cd288740cdb1f685aa8d73de63699ad96
Ilya Zolotov <zolotovil@mail.ru>
2024-06-23 22:52:21 +0300

    Add login_by_personal_area and eddit login_by_loggin_button

--- tests/test_login_by_personal_area_button_from_main_page.py
+++ tests/test_login_by_personal_area_button_from_main_page.py
@@ -7,8 +7,8 @@ from selenium.webdriver.support.wait import WebDriverWait
 driver = webdriver.Chrome()
 driver.get("https://stellarburgers.nomoreparties.site/")
 
-WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable((By.CSS_SELECTOR, ".button_button__33qZ0.button_button_type_primary__1O7Bx.button_button_size_large__G21Vg")))
-driver.find_element(By.CSS_SELECTOR, ".button_button__33qZ0.button_button_type_primary__1O7Bx.button_button_size_large__G21Vg").click()
+WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable((By.XPATH, "//p[@class='AppHeader_header__linkText__3q_va ml-2' and text()='Личный Кабинет']")))
+driver.find_element(By.XPATH, "//p[@class='AppHeader_header__linkText__3q_va ml-2' and text()='Личный Кабинет']").click()
 WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.CSS_SELECTOR, ".Auth_login__3hAey")))
 driver.find_element(By.XPATH, "//input[@class='text input__textfield text_type_main-default' and @name='name']").send_keys("ilya_zolotov_10_555@yandex.ru")
 driver.find_element(By.XPATH, "//input[@class='text input__textfield text_type_main-default' and @name='Пароль']").send_keys("123456")
@@ -17,4 +17,3 @@ WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((
 
 
 driver.quit()
-
