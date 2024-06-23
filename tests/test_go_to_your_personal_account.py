ca861d5e8e6a0f1cb6a518393e18c6fe5a8ec057
Ilya Zolotov <zolotovil@mail.ru>
2024-06-24 00:10:57 +0300

    Create file - go to your personal page

---
+++ tests/test_go_to_your_personal_account.py
@@ -0,0 +1,21 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions
+from selenium.webdriver.support.wait import WebDriverWait
+
+
+driver = webdriver.Chrome()
+driver.get("https://stellarburgers.nomoreparties.site/")
+
+WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable((By.XPATH, "//button[@class='button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_large__G21Vg']")))
+driver.find_element(By.XPATH, "//button[@class='button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_large__G21Vg']").click()
+WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//div[@class='Auth_login__3hAey']")))
+driver.find_element(By.XPATH, "//input[@class='text input__textfield text_type_main-default' and @name='name']").send_keys("ilya_zolotov_10_555@yandex.ru")
+driver.find_element(By.XPATH, "//input[@class='text input__textfield text_type_main-default' and @name='Пароль']").send_keys("123456")
+driver.find_element(By.XPATH, "//button[@class='button_button__33qZ0 button_button_type_primary__1O7Bx button_button_size_medium__3zxIa']").click()
+WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//main[@class='App_componentContainer__2JC2W']")))
+driver.find_element(By.XPATH, "//a[@class='AppHeader_header__link__3D_hX' and contains(@href, '/account')]").click()
+WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable((By.XPATH, "//header[@class='AppHeader_header__X9aJA pb-4 pt-4']")))
+
+driver.quit()
+
