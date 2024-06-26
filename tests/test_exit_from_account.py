480c202d044e44416bea22400457bd3510d79622
Ilya Zolotov <zolotovil@mail.ru>
2024-06-26 16:06:19 +0300

    Edit 3 files - test_registration, test_go_to_buns, test_exit

--- tests/test_exit_from_account.py
+++ tests/test_exit_from_account.py
@@ -17,7 +17,9 @@ WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((
 driver.find_element(By.XPATH, "//a[@class='AppHeader_header__link__3D_hX' and contains(@href, '/account')]").click()
 WebDriverWait(driver, 5).until(expected_conditions.element_to_be_clickable((By.XPATH, "//button[text()='Выход']")))
 driver.find_element(By.XPATH, "//button[text()='Выход']").click()
-WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//div[@class='Auth_login__3hAey']")))
+
+login_text = WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//h2[text()='Вход']"))).text
+assert "Вход" in login_text
 
 driver.quit()
 
--- tests/test_go_to_buns_to_sauces_to_fillings.py
+++ tests/test_go_to_buns_to_sauces_to_fillings.py
@@ -6,26 +6,17 @@ from selenium.webdriver.support.ui import WebDriverWait
 driver = webdriver.Chrome()
 driver.get("https://stellarburgers.nomoreparties.site/")
 
-WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//section[@class='BurgerIngredients_ingredients__1N8v2']")))
-driver.find_element(By.XPATH, "//div/main/section[1]/div[1]/div[2]").click()
-sauces_selector = WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//div[@class='tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']"))).text
-sauces_header = WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//h2[@class='text text_type_main-medium mb-6 mt-10'][2]"))).text
-assert sauces_selector == "Соусы"
-assert sauces_header == "Соусы"
+WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.CLASS_NAME, "BurgerIngredients_ingredients__1N8v2")))
+driver.find_element(By.XPATH, "//span[@class='text text_type_main-default' and contains(text(),'Соусы')]").click()
+sauces_header = WebDriverWait(driver, 5).until(expected_conditions.visibility_of_element_located((By.XPATH, "//h2[contains(@class, 'text text_type_main-medium mb-6 mt-10') and contains(text(), 'Соусы')]")))
+assert "Соусы" in sauces_header.text
 
-driver.find_element(By.XPATH, "//div/main/section[1]/div[1]/div[3]").click()
-fillings_selector = WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//div[@class='tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']"))).text
-fillings_header = WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//h2[@class='text text_type_main-medium mb-6 mt-10'][3]"))).text
+driver.find_element(By.XPATH, "//span[@class='text text_type_main-default' and contains(text(),'Начинки')]").click()
+fillings_header = WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//h2[contains(@class, 'text text_type_main-medium mb-6 mt-10') and contains(text(), 'Начинки')]")))
+assert "Начинки" in fillings_header.text
 
-assert fillings_selector == "Начинки"
-assert fillings_header == "Начинки"
-
-
-driver.find_element(By.XPATH, "//div/main/section[1]/div[1]/div[1]").click()
-buns_selector = WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//div[@class='tab_tab__1SPyG tab_tab_type_current__2BEPc pt-4 pr-10 pb-4 pl-10 noselect']"))).text
-buns_header = WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//h2[@class='text text_type_main-medium mb-6 mt-10'][1]"))).text
-
-assert buns_selector == "Булки"
-assert buns_header == "Булки"
+driver.find_element(By.XPATH, "//span[@class='text text_type_main-default' and contains(text(),'Булки')]").click()
+buns_header = WebDriverWait(driver, 5).until(expected_conditions.presence_of_element_located((By.XPATH, "//h2[contains(@class, 'text text_type_main-medium mb-6 mt-10') and contains(text(), 'Булки')]")))
+assert "Булки" in buns_header.text
 
 driver.quit()
